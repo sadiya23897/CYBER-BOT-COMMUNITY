14382215e331277d2ec18f8c815f4827d2a0c9c6
sadiya23897 <mizanur23892389@gmail.com>
2025-06-21 10:08:28 +0600

    Update bot.js

--- Script/commands/bot.js
+++ Script/commands/bot.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "Obot",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "sadiya",
   description: "goibot",
   commandCategory: "Noprefix",
   usages: "noprefix",
